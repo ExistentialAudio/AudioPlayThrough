229763ffa4b053804693985e067129a1888df392
Devin Roth <devin@devinrothmusic.com>
2022-06-08 14:36:24 -0700

    Initial commit.

---
+++ Sources/RequestMicrophoneAuthorization/include/RequestMicrophoneAuthorization.h
@@ -0,0 +1,15 @@
+//
+//  RequestMicrophoneAuthorization.h
+//  AudioPassThrough
+//
+//  Created by Devin Roth on 2020-07-07.
+//  Copyright © 2020 Existential Audio. All rights reserved.
+//
+
+#ifndef RequestMicrophoneAuthorization_h
+#define RequestMicrophoneAuthorization_h
+
+
+#endif /* RequestMicrophoneAuthorization_h */
+
+void RequestMicrophoneAuthorization(void);
