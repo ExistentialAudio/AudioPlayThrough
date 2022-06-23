f5d0d30f3900cdd6cbd0b1032e08325ab2511d0a
Devin Roth <devinroth@existential.audio>
2022-06-23 09:55:59 -0700

    Add test.

--- Tests/AudioPlayThroughtests/AudioPlayThroughTests.swift
+++ Tests/AudioPlayThroughtests/AudioPlayThroughTests.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import AVFoundation
 import CoreAudio
 import AudioToolbox
 import XCTest
@@ -17,9 +18,23 @@ import AudioPlayThroughObjC
 
 final class TranslateUIDToDeviceTests: XCTestCase {
     
-    let audioPlayThrough = AudioPlayThroughObjC()
+    
     
     func testExample() throws {
+        
+        let audioPlayThrough = AudioPlayThroughObjC()
+
+        XCTAssert(noErr == audioPlayThrough.create(
+            "AppleUSBAudioEngine:Apogee:Quartet:8400000:1,2" as CFString,
+            "AppleUSBAudioEngine:Apogee:Quartet:8400000:1,2" as CFString
+        ))
+        
+        XCTAssert(noErr == audioPlayThrough.start())
+        
+        sleep(10)
+        
+        XCTAssert(noErr == audioPlayThrough.stop())
+        
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
