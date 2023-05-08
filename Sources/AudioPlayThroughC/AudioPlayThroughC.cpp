b5cd13925d5c296b93bf0b50372fcb3cb1855184
Devin Roth <devin@devinrothmusic.com>
2023-05-08 13:57:40 -0700

    Add some extra safety.

--- Sources/AudioPlayThroughC/AudioPlayThroughC.cpp
+++ Sources/AudioPlayThroughC/AudioPlayThroughC.cpp
@@ -16,30 +16,82 @@ void* AudioPlayThroughCreate(CFStringRef inputUID, CFStringRef outputUID)
     
 };
 
+void AudioPlayThroughDestroy(void* audioPlayThrough)
+{
+    if (audioPlayThrough)
+    {
+        delete (AudioPlayThrough*)audioPlayThrough;
+    }
+}
+
 OSStatus AudioPlayThroughStart(void* audioPlayThrough)
 {
-    return ((AudioPlayThrough*)audioPlayThrough)->start();
+    if (audioPlayThrough)
+    {
+        return ((AudioPlayThrough*)audioPlayThrough)->start();
+    }
+
+    return 0;
 };
 
 OSStatus AudioPlayThroughStop(void* audioPlayThrough)
 {
-    return ((AudioPlayThrough*)audioPlayThrough)->stop();
+    if (audioPlayThrough)
+    {
+        return ((AudioPlayThrough*)audioPlayThrough)->stop();
+    }
+
+    
+    return 0;
 };
 
 void AudioPlayThroughSetPeakCallback(void* audioPlayThrough, void(*peakCallback)(Float32 peak))
 {
-    ((AudioPlayThrough*)audioPlayThrough)->peakCallback = peakCallback;
+    if (audioPlayThrough)
+    {
+        ((AudioPlayThrough*)audioPlayThrough)->peakCallback = peakCallback;
+    }
+    
 };
 
 Float32 AudioPlayThroughGetPeak(void* audioPlayThrough)
 {
-    return ((AudioPlayThrough*)audioPlayThrough)->getPeak();
+    if (audioPlayThrough)
+    {
+        return ((AudioPlayThrough*)audioPlayThrough)->getPeak();
+    }
+    
+    return 0;
 };
 
 OSStatus AudioPlayThroughSetMatrixLevel(void* audioPlayThrough, UInt32 inputChannel, UInt32 outputChannel, Float32 level)
 {
-    return ((AudioPlayThrough*)audioPlayThrough)->setMatrixLevel(inputChannel, outputChannel, level);
+    if (audioPlayThrough)
+    {
+        return ((AudioPlayThrough*)audioPlayThrough)->setMatrixLevel(inputChannel, outputChannel, level);
+    }
+    
+    return 0;
 };
+
+void AudioPlayThroughSetAudioUnit(void* audioPlayThrough, AudioUnit audioUnit)
+{
+    if (audioPlayThrough)
+    {
+        ((AudioPlayThrough*)audioPlayThrough)->setAudioUnit(audioUnit);
+    }
+
+    
+}
+
+void AudioPlayThroughBypassAudioUnit(void* audioPlayThrough, UInt32 value)
+{
+    if (audioPlayThrough)
+    {
+        ((AudioPlayThrough*)audioPlayThrough)->bypassAudioUnit(value);
+    }
+}
+
 
 const char* GetBuildDate(){
    return __DATE__;
